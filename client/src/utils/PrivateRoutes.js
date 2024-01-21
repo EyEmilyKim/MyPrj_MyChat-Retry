bf91a5ff2aa783751689c0908971587eb5bea187
EyEmilyKim surf <myue4555@gmail.com>
2024-01-21 22:21:01 +0900

    STYLE (PrivateRoutes) : Animation Loader 적용

--- client/src/utils/PrivateRoutes.js
+++ client/src/utils/PrivateRoutes.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { LoginContext } from '../contexts/LoginContext';
 import { Navigate, Outlet } from 'react-router-dom';
 import { SocketContext } from '../contexts/SocketContext';
+import './PrivateRoutes.css';
 
 const PrivateRoutes = () => {
   const { isLogin, isAuthing } = useContext(LoginContext);
@@ -21,7 +22,9 @@ const PrivateRoutes = () => {
   }, [isAuthing, isConnecting]);
 
   return isAuthing ? (
-    <h1>Loading...</h1>
+    <div className="loader-area">
+      <span className="loader">Load&nbsp;ng</span>
+    </div>
   ) : canNavigate ? (
     isLogin ? (
       <Outlet />
