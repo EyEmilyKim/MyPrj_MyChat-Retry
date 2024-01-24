f229fe5221829c32a9bd9e7e825749ded8a26e83
EyEmilyKim surf <myue4555@gmail.com>
2024-01-24 12:21:03 +0900

    FEAT, STYLE (UserList) : 새로고침 버튼 추가, css / fontAwesome 추가

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
 import './UserList.css';
 import { SocketContext } from '../contexts/SocketContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function UserList() {
   const { userList } = useContext(SocketContext);
@@ -9,25 +11,35 @@ export default function UserList() {
     console.log('userList', userList);
   }, [userList]);
 
+  const refreshThis = () => {
+    window.location.reload();
+  };
+
   return (
     <>
-      <div className="userList-body">
+      <div className="userList-header">
         <h1 className="userList-title">MyApp-test UserList</h1>
+        <div className="refresh-button" onClick={refreshThis}>
+          <FontAwesomeIcon icon={faArrowRotateRight} />
+        </div>
       </div>
 
-      {userList.length > 0
-        ? userList.map((user) => (
-            <div className="user-list" key={user.id}>
-              <div className="user-name">{user.name}</div>
-              <div
-                className={`user-online ${user.online ? 'online' : 'offline'}`}
-              >
-                {user.online ? '●' : '●'}
+      <div className="userList-body">
+        {userList.length > 0
+          ? userList.map((user) => (
+              <div className="each-user" key={user.id}>
+                <div className="user-name">{user.name}</div>
+                <div
+                  className={`user-online ${
+                    user.online ? 'online' : 'offline'
+                  }`}
+                >
+                  {user.online ? '●' : '●'}
+                </div>
               </div>
-              {/* <div className="user-online">{user.online}</div> */}
-            </div>
-          ))
-        : null}
+            ))
+          : null}
+      </div>
     </>
   );
 }
