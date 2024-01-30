2d82dcdba48971e78b80ce6aa9a00280b2c32ec7
EyEmilyKim surf <myue4555@gmail.com>
2024-01-30 21:09:03 +0900

    FEAT (NavBar) : 유저명, 프사(임시) 추가 / REFACTOR :  navItems[] map으로 li 출력

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,18 +1,52 @@
+import { useContext } from 'react';
 import './NavBar.css';
+import { LoginContext } from '../contexts/LoginContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBar() {
+  const { isLogin, user } = useContext(LoginContext);
+
+  const navItems = [
+    {
+      href: '/',
+      text: 'Home',
+    },
+    {
+      href: '/userList',
+      text: 'Users',
+    },
+    {
+      href: '/roomList',
+      text: 'Rooms',
+    },
+  ];
+
   return (
     <div className="nav-container">
+      <div className="nav-user">
+        {isLogin ? (
+          <>
+            <img
+              src="/profile.jpeg"
+              alt="user-profile-image"
+              className="profile-image"
+            />
+            <p className="user-name">{user.name}</p>
+          </>
+        ) : (
+          <FontAwesomeIcon icon={faLock} className="lock-image" />
+        )}
+      </div>
+
       <ul className="nav-list">
-        <li className="nav-items">
-          <a href="/">Home</a>
-        </li>
-        <li className="nav-items">
-          <a href="/userList">Users</a>
-        </li>
-        <li className="nav-items">
-          <a href="/roomList">Rooms</a>
-        </li>
+        {navItems.map((item) => {
+          return (
+            <li className="nav-items" key={item.text}>
+              <a href={item.href}>{item.text}</a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
