60b4f8e825419e6a994f7a8d3642a6d4bac4dec0
EyEmilyKim surf <myue4555@gmail.com>
2024-02-04 21:30:59 +0900

    REFACTOR (Logout) : useLogout 훅 분리. 로그아웃 컨펌 추가.

--- client/src/components/home/Logout.js
+++ client/src/components/home/Logout.js
@@ -1,32 +1,7 @@
-import { useContext } from 'react';
-import { LoginContext } from '../../contexts/LoginContext';
-import axios from 'axios';
-import { handleHttpError } from '../../utils/handleHttpError';
+import useLogout from '../../hooks/useLogout';
 
 export default function Logout() {
-  const { setIsLogin, setUser } = useContext(LoginContext);
-  const apiRoot = process.env.REACT_APP_API_ROOT;
-
-  // 로그아웃 이벤트 처리하는 함수
-  const handleLogout = async () => {
-    // console.log('handleLogout called');
-    try {
-      const res = await axios({
-        url: `${apiRoot}/user/logout`,
-        method: 'POST',
-        withCredentials: true,
-      });
-      if (res.status === 200) {
-        alert(`로그아웃 성공!\n또 만나요 ${res.data.user.name}님~~`);
-        console.log('로그아웃 성공!');
-        setIsLogin(false);
-        setUser(null);
-      }
-    } catch (error) {
-      const notify = true;
-      handleHttpError(error, notify);
-    }
-  };
+  const { handleLogout } = useLogout();
 
   return (
     <div>
---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { LoginContext } from '../contexts/LoginContext';
+import axios from 'axios';
+import { handleHttpError } from '../utils/handleHttpError';
+
+export default function useLogout() {
+  const { setIsLogin, setUser } = useContext(LoginContext);
+  const apiRoot = process.env.REACT_APP_API_ROOT;
+
+  // 로그아웃 이벤트 처리하는 함수
+  const handleLogout = async () => {
+    console.log('handleLogout called');
+    if (window.confirm('로그아웃 하시겠습니까?')) {
+      try {
+        const res = await axios({
+          url: `${apiRoot}/user/logout`,
+          method: 'POST',
+          withCredentials: true,
+        });
+        if (res.status === 200) {
+          alert(`로그아웃 성공!\n또 만나요 ${res.data.user.name}님~~`);
+          console.log('로그아웃 성공!');
+          setIsLogin(false);
+          setUser(null);
+        }
+      } catch (error) {
+        const notify = true;
+        handleHttpError(error, notify);
+      }
+    }
+  };
+
+  return {
+    handleLogout,
+  };
+}
