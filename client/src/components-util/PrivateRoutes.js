462a3e61b1a3be65ad0e1529ee5521097e181eda
EyEmilyKim surf <myue4555@gmail.com>
2024-02-05 18:00:39 +0900

    FIX (PrivateRoutes) : canNavigate 조건 추가 (인증 후 비로그인 시 이동)

--- client/src/components-util/PrivateRoutes.js
+++ client/src/components-util/PrivateRoutes.js
@@ -6,12 +6,20 @@ import useStateLogger from '../hooks/useStateLogger';
 import Loader from './Loader';
 
 const PrivateRoutes = () => {
-  const { isLogin } = useContext(LoginContext);
+  const { isLogin, isAuthing } = useContext(LoginContext);
   const { isConnecting } = useContext(SocketContext);
   const [canNavigate, setCanNavigate] = useState(false);
   // useStateLogger(canNavigate, 'canNavigate');
 
   useEffect(() => {
+    // 인증 후 비로그인 시 이동
+    if (!isAuthing && !isLogin) {
+      setCanNavigate(true);
+    }
+  }, [isAuthing]);
+
+  useEffect(() => {
+    // 소켓 연결 후 이동
     if (!isConnecting) {
       setCanNavigate(true);
     }
