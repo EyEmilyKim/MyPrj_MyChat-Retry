9267b72e89553a97f7433ac08aae4cfb98d9d3f2
EyEmilyKim surf <myue4555@gmail.com>
2024-02-07 20:46:25 +0900

    FEAT (message) : index 필드 추가, pre-save 로 자동증가 값 할당

--- server/models/message.js
+++ server/models/message.js
@@ -19,6 +19,22 @@ const MessageSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  index: {
+    type: Number,
+    required: true,
+    default: -1,
+  },
+});
+
+MessageSchema.pre('save', async function (next) {
+  // index 값 자동 생성(auto increase)
+  if (!this.index || this.index < 0) {
+    const latestMessage = await this.constructor
+      .findOne({ room: this.room })
+      .sort('-index');
+    this.index = (latestMessage ? latestMessage.index : 0) + 1;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
