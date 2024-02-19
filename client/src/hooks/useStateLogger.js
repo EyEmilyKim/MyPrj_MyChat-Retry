7ac43c00bd05ca0204d62d9e964d3874932a50e6
EyEmilyKim surf <myue4555@gmail.com>
2024-02-19 16:52:57 +0900

    FIX (useStateLogger) : 용도 단순화 -> state 하나씩만 받기 (배열 state도 그냥 log 한줄로)

--- client/src/hooks/useStateLogger.js
+++ client/src/hooks/useStateLogger.js
@@ -2,12 +2,6 @@ import React, { useEffect } from 'react';
 
 export default function useStateLogger(state, stateName) {
   useEffect(() => {
-    if (Array.isArray(state)) {
-      state.forEach((item, index) => {
-        console.log(`[${stateName[index]}]`, item);
-      });
-    } else {
-      console.log(`[${stateName}]`, state);
-    }
+    console.log(`[${stateName}]`, state);
   }, [state]);
 }
